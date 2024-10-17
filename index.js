d7774051100be2284d52e0023001f9a0bf053587
adam tarango <you@example.comadam.tarango97@gmail.comgit commitgit statusgit statusgit config --global user.email you@example.comadam.tarango97@gmail.com>
2024-10-17 14:11:58 -0700

    add randomizer

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const freelancers = [
   { name: "Alice", price: 30, occupation: "Writer" },
   { name: "Bob", price: 50, occupation: "Teacher" },
   { name: "Carol", price: 70, occupation: "Programmer" },
-  { name: "Dr. Slice", price: 25, occupation: "gardener" },
-  { name: "Dr. Pressure", price: 51, occupation: "programmer" },
-  { name: "Prof. Possibility", price: 43, occupation: "teacher" },
-  { name: "Prof. Prism", price: 81, occupation: "teacher" },
-  { name: "Dr. Impulse", price: 43, occupation: "teacher" },
-  { name: "Prof. Spark", price: 76, occupation: "programmer" },
-  { name: "Dr. Wire", price: 47, occupation: "teacher" },
-  { name: "Prof. Goose", price: 72, occupation: "driver" },
 ];
+const names = [`Mark`,`Christine`,`Colt`,`Quinten`,`Nyah`,`Alize`,`Kalen`,`Allissa`,`Anissa`,`Mikaila`,`Dana`,`Lucille`,`Robyn`,`River`,`Debra`,`India`,`Jeniffer`,`Sierra`,`Jordan`,`Charlie`,`Joan`,`Magdalena`,`Truman`,`Skyla`,`Adelaide`,`Sage`,`Darian`,`Lucero`,`Austin`,`Trenten`,`Lukas`,`Abbigail`,`Amir`,`Cooper`,`Khadijah`,`Craig`,`Axel`,`Sanjana`,`Amya`,`Timmy`,`Farrah`,`Josie`,`Mitchel`,`Karolina`,`Rodrigo`,`Destinie`,`Cedric`,`Mckenzie`,`Marissa`,`Chyna`,`Jessalyn`,`Jadon`,`Malachi`,`Malcolm`,`Luisa`,`Rocco`,`Estefany`,`Colette`,`Ronaldo`,`Marcela`,`Ali`,`Jaqueline`,`Jeff`,`Sawyer`,`Phillip`,`Jazlynn`,`Keona`,`Lynsey`,`Nathan`,`Harris`,`Shauna`,`Heidi`,`Hakeem`,`Isabella`,`Miracle`,`Alina`,`Deana`,`Angela`,`Tiera`,`Gianni`,`Karson`,`Madilynn`,`Natalia`,`Melinda`,`Stephan`,`Laney`,`Tayla`,`Sabrina`,`Lilly`,`Jania`,`Destini`,`Kade`,`Anais`,`Corbin`,`Colby`,`Amari`,`Jayme`,`Brian`,`Lena`,`Ryder`,`Samir`]
+const jobs = [`Pilot`,`Chef`,`Curator`,`Football`,`Also Pilot`,`Not a Pilot`,`Word`,`Placeholder`,`700`,`Tuesday`]
+const randFree = (nameList, jobList) =>{
+  const name = nameList[Math.floor(Math.random()*nameList.length)];
+  const job = jobList[Math.floor(Math.random()*jobList.length)];
+  cost = Math.floor(Math.random()*100);
+  const randPers = {name: name, price: cost, occupation: job}
+  return randPers
+}
 const rollingAvg = (currAvgArr, newNum) => {
   currAvgArr.push(newNum)
   const sum = currAvgArr.reduce((sum, num) => {return sum+num}, 0)
@@ -45,9 +46,8 @@ addEntry(freelancers[1], prices)
 addEntry(freelancers[2], prices)
 let i = 3
 const interval = setInterval(() => {
-addEntry(freelancers[i], prices);
+  freelancers.push (randFree(names, jobs))
+  addEntry(freelancers[i], prices);
 i += 1;
-if (i === freelancers.length) {
-  return
 }
-}, 30)
+, 3000)
